ce43e00272eef8703e10f651286c666b55585cc4
Your Name <bsmit339@gmail.com>
2025-10-27 11:04:30 -0500

    Simplify search code
    - Shorten variable names (q instead of search_query)
    - Compact query formatting
    - Simplify placeholder text
    - Clean up HTML formatting

--- app.py
+++ app.py
@@ -53,20 +53,19 @@ def index():
             except ValueError:
                 flash('Year must be a valid number.', 'error')
     
-    search_query = request.args.get('search', '').strip()
+    q = request.args.get('search', '').strip()
     
-    if search_query:
+    if q:
+        search = f'%{q}%'
         vinyls = Vinyl.query.filter(
-            db.or_(
-                Vinyl.title.ilike(f'%{search_query}%'),
-                Vinyl.artist.ilike(f'%{search_query}%'),
-                Vinyl.genre.ilike(f'%{search_query}%')
-            )
+            db.or_(Vinyl.title.ilike(search), 
+                   Vinyl.artist.ilike(search), 
+                   Vinyl.genre.ilike(search))
         ).order_by(Vinyl.date_added.desc()).all()
     else:
         vinyls = Vinyl.query.order_by(Vinyl.date_added.desc()).all()
     
-    return render_template('index.html', vinyls=vinyls, search_query=search_query)
+    return render_template('index.html', vinyls=vinyls, search_query=q)
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
